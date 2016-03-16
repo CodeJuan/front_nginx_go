3b65e44000d332b5d958f00d67ed0fc424141086
codejuan <xh@decbug.com>
2016-03-16 13:34:06 +0800

    add getdata

--- front/assets/js/app.js
+++ front/assets/js/app.js
@@ -20,10 +20,20 @@ testApp.controller('testController', function($scope) {
     $scope.pageClass = 'test';
 });
 
-testApp.controller('getController', function($scope) {
+testApp.controller('getController', function($scope, $http) {
     $scope.pageClass = 'get';
+
+        $scope.getdata = function() {
+            $scope.data = [];
+            $http.get('api/v1/user/' + $scope.input_name)
+                .success(function(result) {
+                    $scope.datas = result
+                    assert(result)
+                }
+            );
+        };
 });
 
-testApp.controller('postController', function($scope) {
+testApp.controller('postController', function($scope, $http) {
     $scope.pageClass = 'post';
 });
